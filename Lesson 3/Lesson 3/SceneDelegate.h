492dbc84c2cc38861180ee61ea416b94b8112876
Fierrasgen <Look00@inbox.ru>
2021-04-13 12:39:59 +0300

    # - add lesson 3

---
+++ Lesson 3/Lesson 3/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Lesson 3
+//
+//  Created by Alexander Novikov on 09.04.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Lesson 3/Lesson 3/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Lesson 3
+//
+//  Created by Alexander Novikov on 09.04.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
